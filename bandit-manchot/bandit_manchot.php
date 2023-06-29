d9339e2da2c5043fc2923afd2f1d366ef5ada6f4
Tony Blard <tonyblard55@gmail.com>
2023-06-29 21:04:43 +0200

    Feat : Create first page

--- bandit-manchot/bandit_manchot.php
+++ bandit-manchot/bandit_manchot.php
@@ -6,4 +6,43 @@
    Author: SALHI Karim and BLARD Tony
    License: MIT
    */
+
+   class BanditManchotPlugin
+   {
+    public function __construct()
+    {
+        
+        add_action( 
+            'admin_menu', 
+            [$this, 'banditMenu']
+        );
+
+        add_action(
+            'admin_init',
+            [$this, 'banditInit']
+        );
+    }
+    private function banditMenu() {
+        add_menu_page(
+            'Bandit Manchot',
+            'Bandit Manchot', // menu_title
+            'manage_options', // capability
+            'bandit-manchot', // menu_slug
+            [$this, 'my_custom_menu_page'], // function
+            'dashicons-money', // icon_url
+            15 // position
+        );
+    }
+
+    private function banditInit() {
+
+    }
+
+    private function my_custom_menu_page(){
+        esc_html_e( 'Admin Page Test', 'textdomain' );	
+    }
+   }
+
+   if ( is_admin() )
+        $multi_armed_bandit = new BanditManchotPlugin();
 ?>
